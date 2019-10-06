d2bb9fc01b8ef98068aebfe99c8a20debfa04591
Glitch (hello-express) <none>
2019-10-06 14:15:52 +0000

    🎌🌾 Checkpoint ./config.js:6703822/87

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'Yout Bot Token';
+exports.TOKEN = 'NTkwOTA0MzkyOTAzOTUwNTEw.XY0RwA.uUhGHhm0n3OEk3A-DWYLCddJVFM';
 
-exports.PREFIX = 'Your Prefix';
+exports.PREFIX = '*';
 
 exports.GOOGLE_API_KEY = 'AIzaSyDEJleTP57rVf7sNVGzMhEtte2zxNXBsrE';
