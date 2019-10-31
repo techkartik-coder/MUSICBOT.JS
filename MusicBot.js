00b2695fafe5c63e98f4239dd6d16ec02a795acf
Glitch (truth-shawl) <none>
2019-10-31 07:39:41 +0000

    🎏🏰 Checkpoint ./MusicBot.js:6703822/319

--- MusicBot.js
+++ MusicBot.js
@@ -17,7 +17,7 @@ const youtube = new YouTube(GOOGLE_API_KEY);
 const queue = new Map(); 
 
 client.on('message', message => {
-  if (message.content === '&!ping') {
+  if (message.content === PREFIX + 'ping') {
     message.reply('pong');
   }
 });
@@ -112,6 +112,10 @@ client.on('message', async msg => { // eslint-disable-line
             return handleVideo(video, msg, voiceChannel);
         }
 	} else if (command === 'skip') {
+    if (!msg.member.hasPermission("ADMINISTRATOR")) {
+      return msg.reply('YOU DIDENT HAVE ADMINISTRATOR PERMISSIONS!')
+    }
+    
 		if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
 		if (!serverQueue) return msg.channel.send('There is nothing playing that I could skip for you.');
 		serverQueue.connection.dispatcher.end('Skip command has been used!');
