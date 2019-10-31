2db39cd69759ad7b9d999939a96d82e6762bd270
Glitch (truth-shawl) <none>
2019-10-31 10:40:22 +0000

    🚵✌️ Checkpoint ./MusicBot.js:6703822/114

--- MusicBot.js
+++ MusicBot.js
@@ -127,7 +127,6 @@ client.on('message', async msg => { // eslint-disable-line
 		if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
 		if (!serverQueue) return msg.channel.send('There is nothing playing that I could skip for you.');
 		serverQueue.connection.dispatcher.end('Skip command has been used!');
-    msg.channel.send('
 		return undefined;
 	} else if (command === 'stop') {
         if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
