b6534332f1e72334431660d90d5bde9a80d6aea9
Glitch (truth-shawl) <none>
2019-10-31 08:04:01 +0000

    🎀🛳 Checkpoint ./MusicBot.js:6703822/518 ./config.js:6703822/288

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 exports.TOKEN = 'NjM2OTc4ODEwNDU3MjkyODQw.XbKHAA.WnWY-b0BzlyfOvatgg9mAIVFo6Q';
 exports.PREFIX = '!';
+let PREFIX = '!';
 
 exports.GOOGLE_API_KEY = 'AIzaSyC8GHxFJ17uRpUYuyUngZ4lrlWNV_P85-w';
+
+client.on('message', message => {
+  if(mesage.content === '!prefix') {
+    
