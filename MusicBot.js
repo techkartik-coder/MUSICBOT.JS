e9dd00563e4fc5cc8e94746d9d53a266e6d61204
Glitch (truth-shawl) <none>
2019-11-16 15:06:38 +0000

    🚋💒 Checkpoint ./MusicBot.js:5790654/836

--- MusicBot.js
+++ MusicBot.js
@@ -124,6 +124,8 @@ client.on('message', async msg => { // eslint-disable-line
       return msg.reply('YOU DIDENT HAVE ADMINISTRATOR PERMISSIONS!')
     }
     
+    
+    
 		if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
 		if (!serverQueue) return msg.channel.send('There is nothing playing that I could skip for you.');
 		serverQueue.connection.dispatcher.end('Skip command has been used!');
